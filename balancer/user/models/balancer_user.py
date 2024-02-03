9d25f95ab80c5ea93c58f014f2a055c9a13759cd
ruspa <r.uspasskij@cism-ms.ru>
2024-02-03 18:41:11 +0300

    Добавлено поле "created_at"

--- balancer/user/models/balancer_user.py
+++ balancer/user/models/balancer_user.py
@@ -5,7 +5,7 @@ from django.utils import timezone
 class BalancerUser(models.Model):
     tg_token = models.CharField(max_length=256, unique=True, null=False, blank=False)
     date_joined = models.DateTimeField(default=timezone.now)
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f'user_id: {self.pk}'
-
