790770a5d13ae0090dbbc550ac00e3642187aa24
ruspa <r.uspasskij@cism-ms.ru>
2024-02-03 18:43:50 +0300

    Создан ViewSet для юзера

--- balancer/user/views.py
+++ balancer/user/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.response import Response
 
-# Create your views here.
+from user.models import BalancerUser
+from user.serializers import BalancerUserSerializer
+
+
+class UserViewSet(viewsets.ViewSet):
+    queryset = BalancerUser.objects.all()
+    serializer_class = BalancerUserSerializer()
+
+    def create(self, request):
+        """Это HTTP метод POST"""
+        serializer = BalancerUserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=201)
+        else:
+            return Response(data=serializer.errors, status=400)
