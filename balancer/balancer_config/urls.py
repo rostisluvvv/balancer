18ac359df1572f867d779fb606d472d69e3f1dcd
ruspa <r.uspasskij@cism-ms.ru>
2024-02-03 18:40:31 +0300

    Добавлен URL юзера

--- balancer/balancer_config/urls.py
+++ balancer/balancer_config/urls.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+API_PREFIX = "api/v1/"
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path(API_PREFIX, include("user.urls")),
+
 ]
